a243b9bd40e6eb7e2ced5f21cd1f7fecbbc896a3
vagrant <vagrant@vagrant.vm>
2018-07-05 05:38:59 +0000

    add blacklist

--- contracts/OreOreCoin.sol
+++ contracts/OreOreCoin.sol
@@ -6,8 +6,16 @@ contract OreOreCoin {
     uint8 public decimals;
     uint256 public totalSupply;
     mapping (address => uint256) public balanceOf;
+    mapping (address => int8) public blackList;
+    address public owner;
+
+    modifier onlyOwner() {if (msg.sender != owner ) revert(); _;}
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Blacklisted(address indexed target);
+    event DeleteFromBlacklist(address indexed target);
+    event RejectedPaymentToBlacklistedAddr(address indexed from, address indexed to, uint256 value);
+    event RejectedPaymentFromBlacklistedAddr(address indexed from, address indexed to, uint256 value);
 
     constructor(uint256 _supply, string _name, string _symbol, uint8 _decimals) public {
         balanceOf[msg.sender] = _supply;
@@ -15,15 +23,31 @@ contract OreOreCoin {
         symbol = _symbol;
         decimals = _decimals;
         totalSupply = _supply;
+        owner = msg.sender;
     }
 
-    function transafer(address _to, uint256 _value) public {
+    function blacklisting(address _addr) onlyOwner public {
+        blackList[_addr] = 1;
+        emit Blacklisted(_addr);
+    }
+
+    function deleteFromBlacklist(address _addr) onlyOwner public {
+        blackList[_addr] = -1;
+        emit DeleteFromBlacklist(_addr);
+    }
+
+    function transfer(address _to, uint256 _value) public {
         if (balanceOf[msg.sender] < _value) revert();
         if (balanceOf[_to] + _value < balanceOf[_to]) revert();
 
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
+        if (blackList[msg.sender] > 0) {
+            emit RejectedPaymentFromBlacklistedAddr(msg.sender, _to, _value);
+        } else if (blackList[_to] > 0) {
+            emit RejectedPaymentToBlacklistedAddr(msg.sender, _to, _value); 
+        } else {
+            balanceOf[msg.sender] -= _value;
 
-        emit Transfer(msg.sender, _to, _value);
+            emit Transfer(msg.sender, _to, _value);
+        }
     }
 }
